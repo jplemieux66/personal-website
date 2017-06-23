35fe2d07b5dbf6563c2c592beb83a3dd8f9f434e
JP <phillemieux66@hotmail.com>
2017-06-23 13:55:14 -0400

    Upgrade to react-router 4

--- src/App.js
+++ src/App.js
@@ -1,22 +1,15 @@
+// React
 import React, { Component } from 'react';
-import {Route, Router, IndexRoute, browserHistory} from 'react-router';
+// React-Router
+import {Route, BrowserRouter as Router} from 'react-router-dom';
+// Components
 import MainContainer from './Components/MainContainer/MainContainer';
-import HomePage from './Components/HomePage/HomePage';
-import AboutPage from './Components/AboutPage/AboutPage';
-import EducationAndSkillsPage from './Components/EducationAndSkillsPage/EducationAndSkillsPage';
-import PreviousWorkPage from './Components/PreviousWorkPage/PreviousWorkPage';
-import './App.css';
 
 class App extends Component {
   render () {
     return (
-      <Router history={browserHistory}>
-        <Route path="/" component={MainContainer}>
-          <IndexRoute component={HomePage} />
-          <Route path="about" component={AboutPage}/>
-          <Route path="education-and-skills" component={EducationAndSkillsPage} />
-          <Route path="work" component={PreviousWorkPage} /> 
-        </Route>
+      <Router>
+        <Route path="/" component={MainContainer} />
       </Router>
     );
   }
--- src/Components/MainContainer/MainContainer.js
+++ src/Components/MainContainer/MainContainer.js
@@ -1,6 +1,11 @@
 import React, { Component } from 'react';
+import {Route} from 'react-router-dom';
 import Navbar from './../Navbar/Navbar';
 import ContactFooter from './../ContactFooter/ContactFooter';
+import HomePage from './../HomePage/HomePage';
+import AboutPage from './../AboutPage/AboutPage';
+import EducationAndSkillsPage from './../EducationAndSkillsPage/EducationAndSkillsPage';
+import PreviousWorkPage from './../PreviousWorkPage/PreviousWorkPage';
 import './MainContainer.css';
 
 class MainContainer extends Component {
@@ -9,7 +14,10 @@ class MainContainer extends Component {
       <div>
         <Navbar />
         <div className="container">
-          {this.props.children}
+          <Route exact path="/" component={HomePage} />
+          <Route path="/about" component={AboutPage}/>
+          <Route path="/education-and-skills" component={EducationAndSkillsPage} />
+          <Route path="/work" component={PreviousWorkPage} />
         </div>
         <ContactFooter />
       </div>
