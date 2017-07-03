743b19cf3af8a127822fce0a5505ed26b5d1f816
JP <phillemieux66@hotmail.com>
2017-07-03 09:10:39 -0400

    Add Footer Draft

--- src/Components/ContactFooter/ContactFooter.js
+++ src/Components/ContactFooter/ContactFooter.js
@@ -4,7 +4,19 @@ import './ContactFooter.css';
 class ContactFooter extends Component {
   render() {
     return (
-      <h1>Contact Footer</h1>
+      <div id="contact-footer">
+        <div id="contact-links">
+          <a className="contact-link" href="https://www.linkedin.com/in/jean-philippe-lemieux-b62b77b0" target="_blank" rel="noopener noreferrer">
+            <i className="contact-icon fa fa-linkedin-square fa-3x" aria-hidden="true"/>LinkedIn
+          </a>
+          <a className="contact-link" href="https://github.com/jplemieux66" target="_blank" rel="noopener noreferrer">
+            <i className="contact-icon fa fa-github-square fa-3x" aria-hidden="true"/>Github
+          </a>
+          <a className="contact-link" href="mailto:jean-philippe.lemieux@polymtl.ca">
+            <i className="contact-icon fa fa-envelope fa-3x" aria-hidden="true"/>Email
+          </a>
+        </div>
+      </div>
     );
   }
 }
